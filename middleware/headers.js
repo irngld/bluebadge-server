2eff2fb1a6aeb9ad77eca579428ba37f81d33210
irngld <iaingould1@gmail.com>
2021-05-25 22:09:24 -0400

    working on favorites, adding to DB

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,9 +1,12 @@
 const headers = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers')
+    res.header('Access-Control-Allow-Credentials', true)
 
     next()
 }
+
+
 
 module.exports = headers;
--- middleware/validateSession.js
+++ middleware/validateSession.js
@@ -4,13 +4,17 @@ const User = require('../models/user');
 const validateSession = (req, res, next) => {
     const token = req.headers.authorization;
 
+    console.log("validateSession", req.headers);
+
     if (!token) {
+        console.log("token:", token);
         return res.status(403).json({
             auth: false,
             message: 'No token provided'
         })
     }
     else {
+        console.log(token);
         jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
             if (!err && decodedToken) {
                 User.findOne({
@@ -27,6 +31,7 @@ const validateSession = (req, res, next) => {
                 .catch(err => next(err));
             }
             else {
+                console.log(err);
                 req.errors = err;
                 return res.status(500).send('Not Authorized');
             }
