a7f0494afb157056ba64a1f35186a6ce744c2b5f
irngld <iaingould1@gmail.com>
2021-06-05 13:02:14 -0400

    updated db ssl settings

--- db.js
+++ db.js
@@ -2,6 +2,15 @@ const Sequelize = require("sequelize");
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  ssl: true,
+  protocol: "postgres",
+  logging: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = db;
